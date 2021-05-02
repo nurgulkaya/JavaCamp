167872b47e774558c2f2ce4e9ee52974759bf98e
Nurgül <nnurgulky@gmail.com>
2021-05-02 22:21:32 +0300

    Update InstructorManager.java

--- homeworkday3/src/homeworkday3/InstructorManager.java
+++ homeworkday3/src/homeworkday3/InstructorManager.java
@@ -3,7 +3,7 @@ package homeworkday3;
 public class InstructorManager {
       public void teachCourses(Instructor instructor) {
     	  System.out.println(instructor.getFirstName() + instructor.getLastName() + 
-    			  instructor.getCoursesInstructor() + " eðitimi verecek.");
+    			  instructor.getCoursesInstructor() + " eÄŸitimi verecek.");
       }
 
 	
