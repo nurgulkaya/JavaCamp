4c706bac7ebe0c26f852b8e2992bad2693264377
Nurgül <nnurgulky@gmail.com>
2021-05-08 17:51:48 +0300

    Update Main.java

--- homeworkday4_1/src/homeworkday4_1/Main.java
+++ homeworkday4_1/src/homeworkday4_1/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
 	public static void main(String[] args) {
     
-       BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-	   customerManager.Save( new Customer (1, "Nurgül", "Kaya", 2001, "13858498578"));
+           BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+	      customerManager.Save( new Customer (1, "NurgÃ¼l", "Kaya", 2001, "11111111111"));
        
        
 	}
